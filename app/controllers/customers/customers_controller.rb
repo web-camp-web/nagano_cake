db2fab66ed4623440bb94505b64a0eb1965c1428
tatsumura <m.tatsuya6825@gmail.com>
2021-05-17 02:36:26 +0000

    [update]customers/customers_controller action

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,10 +1,35 @@
 class Customers::CustomersController < ApplicationController
+  
   def show
-  end
-
-  def destroy_confirm
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_customer_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy_confirm
+    @customer = current_customer
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+    end
 end
