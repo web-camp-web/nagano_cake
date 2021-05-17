0f695280cd7d24d4427bc17c8d9ca57d63f1ee7c
tatsumura <m.tatsuya6825@gmail.com>
2021-05-17 02:44:09 +0000

    [update]customers/customers show page

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -11,7 +11,7 @@ class Customers::CustomersController < ApplicationController
   def update
     @customer = current_customer
     if @customer.update(customer_params)
-      redirect_to customers_customer_path
+      redirect_to customer_path
     else
       render :edit
     end
