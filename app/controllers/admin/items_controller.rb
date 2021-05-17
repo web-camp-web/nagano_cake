fdc13c3d4372d5420fe5a26124b7eebcbdfdd37e
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-05-17 02:23:19 +0000

    [feat]genre page

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,37 @@
 class Admin::ItemsController < ApplicationController
-  def index
+ 
+  def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
-  def new
+  def create
+    @item = Item.new(params_item)
+    @item.save
+    redirect_to admins_items_path
+  end
+
+  def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    Item.find(params[:id]).update(params_item)
+    redirect_to admins_items_path
+  end
+
+  private
+
+  def params_item
+    params.require(:item).permit(:name, :caption, :price, :genre_id, :is_active, :image_id)
   end
 end
