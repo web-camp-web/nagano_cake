8fc0db61a5d95d094ddb8b5c69ba88645a391710
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-05-17 08:19:22 +0000

    [change]header cart_item item,cart_item,genre-autenticate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
   has_many :oreders, through: :order_items
   has_many :order_items
   belongs_to :genre
