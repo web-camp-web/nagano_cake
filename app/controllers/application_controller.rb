7d534d0056f3bf5fb56b042df79dffd9f6cf6ab3
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-19 06:52:23 +0000

    [add]rspec_admin/items_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,15 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_out_path_for(resource)
+    case resource
+    when Admin
+      new_admin_session_path
+    when Customer
+      root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- spec/system/01_admin_from_login_to_item_registration_spec.rb
+++ spec/system/01_admin_from_login_to_item_registration_spec.rb
@@ -63,14 +63,108 @@ describe '1.マスタ登録のテスト' do
 
     it '必要事項を入力して登録ボタンを押すと登録した商品の詳細画面が表示される' do
       attach_file 'item[image]', "#{Rails.root}/app/assets/images/strawberry-cake.jpg"
-      fill_in 'item[name]', with: 'いちごのショートケーキ(ホール)'
+      fill_in 'item[name]', with: 'いちごのケーキ(ホール)'
       fill_in 'item[caption]', with: '高級いちごを贅沢に使用しています'
       select genre.name, from: 'item[genre_id]'
       fill_in 'item[price]', with: 3000
-      #check 'item[is_active]'
       click_button '送信'
       expect(page).to have_content '商品詳細'
-      expect(page).to have_content 'いちごのショートケーキ(ホール)'
+      expect(page).to have_content 'いちごのケーキ(ホール)'
+    end
+  end
+
+  context '商品詳細画面のテスト' do
+    let!(:genre) { create(:genre) }
+    let!(:item) { create(:item) }
+
+    before do
+      visit admin_item_path(item)
+    end
+
+    it 'ヘッダーに「商品一覧」というリンクがある' do
+      expect(page).to have_link '商品一覧'
+    end
+
+    it '「商品一覧」のリンクを押すと商品一覧画面が表示される' do
+      click_link '商品一覧'
+      expect(current_path).to eq admin_items_path
+    end
+  end
+
+  context '商品一覧画面(1商品登録後)のテスト' do
+    let!(:genre) { create(:genre) }
+    let!(:item) { create(:item) }
+
+    before do
+      visit admin_items_path
+    end
+
+    it '登録した商品が表示されている' do
+      expect(page).to have_content item.name and item.price and item.genre and item.is_active
+    end
+
+    it '「商品作成はこちら」というリンクを押すと商品新規登録画面が表示される' do
+      click_link '商品作成はこちら'
+      expect(current_path).to eq new_admin_item_path
+    end
+  end
+
+  context '商品新規登録(2商品目)のテスト' do
+    let!(:genre) { create(:genre) }
+    let!(:item) { create(:item) }
+
+    before do
+      visit new_admin_item_path
+    end
+
+    it '必要事項を入力して登録ボタンを押すと登録した商品の詳細画面が表示される' do
+      attach_file 'item[image]', "#{Rails.root}/app/assets/images/strawberry-cake.jpg"
+      fill_in 'item[name]', with: 'りんごのケーキ(ホール)'
+      fill_in 'item[caption]', with: '高級りんごを贅沢に使用しています'
+      select genre.name, from: 'item[genre_id]'
+      fill_in 'item[price]', with: 2500
+      click_button '送信'
+      expect(page).to have_content '商品詳細'
+      expect(page).to have_content 'りんごのケーキ(ホール)'
+    end
+  end
+
+  context '商品詳細画面(2商品登録後)のテスト' do
+    let!(:genre) { create(:genre) }
+    let!(:item_1) { create(:item) }
+    let!(:item_2) { create(:item) }
+
+    before do
+      visit admin_item_path(item_2)
+    end
+
+    it 'ヘッダーに「商品一覧」というリンクがある' do
+      expect(page).to have_link '商品一覧'
+    end
+
+    it '「商品一覧」のリンクを押すと商品一覧画面が表示される' do
+      click_link '商品一覧'
+      expect(current_path).to eq admin_items_path
+    end
+  end
+
+  context '商品一覧画面(2商品登録後)のテスト' do
+    let!(:genre) { create(:genre) }
+    let!(:item_1) { create(:item) }
+    let!(:item_2) { create(:item) }
+
+    before do
+      visit admin_items_path
+    end
+
+    it '登録した商品が2つとも表示されている' do
+      expect(page).to have_content item_1.name and item_1.price and item_1.genre and item_1.is_active
+      expect(page).to have_content item_2.name and item_2.price and item_2.genre and item_2.is_active
+    end
+
+    it '「ログアウト」リンクを押すと管理者ログイン画面に遷移する' do
+      click_link 'ログアウト'
+      expect(current_path).to eq new_admin_session_path
     end
   end
 end
